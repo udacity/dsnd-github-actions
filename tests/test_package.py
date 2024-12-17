0cb009efe6f4a31d3628016e92a692bb13fc347d
Jo-L <joelswhere@gmail.com>
2024-12-17 07:40:31 +0000

    linting

--- tests/test_package.py
+++ tests/test_package.py
@@ -1,9 +1,8 @@
 def test_package_import():
     import package_name  # noqa: F401
 
+
 def test_failure():
     from package_name import a_function_that_will_break
 
     a_function_that_will_break()
-
-

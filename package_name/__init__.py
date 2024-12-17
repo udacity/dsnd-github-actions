f9c18d5cc75387c6c953a39956ce16c90961224f
Jo-L <joelswhere@gmail.com>
2024-12-17 07:38:08 +0000

    breaking test

---
+++ package_name/__init__.py
@@ -0,0 +1,2 @@
+def a_function_that_will_break():
+    assert False
--- tests/test_package.py
+++ tests/test_package.py
@@ -1,2 +1,9 @@
 def test_package_import():
     import package_name  # noqa: F401
+
+def test_failure():
+    from package_name import a_function_that_will_break
+
+    a_function_that_will_break()
+
+
